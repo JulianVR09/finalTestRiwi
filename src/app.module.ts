e64c21d200495487ec07dbf2406f24e0f1f6164c
JulianVR09 <julianvillacis.02@gmail.com>
2024-10-07 17:29:44 -0500

    chore(user): update the user entity to control it from auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { UserModule } from './user/user.module';
         username: configService.get<string>('DATABASE_USER'),
         password: configService.get<string>('DATABASE_PASSWORD'),
         database: configService.get<string>('DATABASE_NAME'),
-        entities: ['src/**/*.entity.ts'],
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
       inject: [ConfigService],
