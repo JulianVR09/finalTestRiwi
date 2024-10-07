0626b41f4fe607557e1cf3f020b9eb02846d646f
JulianVR09 <julianvillacis.02@gmail.com>
2024-10-07 16:06:43 -0500

    initial commit: First commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: ['src/**/*.entity.ts'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v0')
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('SimulacroMySQL')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+
+    const documnet = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, documnet)
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
